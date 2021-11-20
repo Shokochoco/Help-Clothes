046fea19d7ab3391f592afacbcb114f59f6bb7bc
Shokochoco <shsuzu1992@gmail.com>
2021-11-20 16:08:17 +0900

    Initial Commit

---
+++ Help clothes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Help clothes
+//
+//  Created by 鈴木淳子 on 2021/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
